05e4ef688a0b3fa940c165d5cbfc6121d764fd99
Andrei Dubouski <dubouski.andrei@gmail.com>
2019-04-02 00:43:59 +0300

    Fix method 'task_1' for HW_03

--- HW_03/Andrei_Dubouski/andrei_dubouski_hw_03_t_01.rb
+++ HW_03/Andrei_Dubouski/andrei_dubouski_hw_03_t_01.rb
@@ -1,7 +1,5 @@
 def task_1(str)
-  result = ''
-  if str.is_a?(String) && str.downcase.include?('error')
-    result = str
+  str.each_line do |line|
+    return line ? line.is_a?(String) && line.downcase.include?('error') : ''
   end
-  return result
 end
