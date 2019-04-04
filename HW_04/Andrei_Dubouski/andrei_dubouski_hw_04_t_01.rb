ec70dc8aa23a488577dc173b4bef876656f76dc8
Andrei Dubouski <dubouski.andrei@gmail.com>
2019-04-04 13:23:34 +0300

    Refactor method 'task_4_1_b' for HW_04

--- HW_04/Andrei_Dubouski/andrei_dubouski_hw_04_t_01.rb
+++ HW_04/Andrei_Dubouski/andrei_dubouski_hw_04_t_01.rb
@@ -12,28 +12,19 @@ end
 def task_4_1_a(number)
   # used recursion
   res = 0
-  if number.is_a?(Integer)
-    res = fib(number)
-  end
-  res
+  number.is_a?(Integer) ? fib(number) : res
 end
 
 def task_4_1_b(number)
   # used dynamic programming
   result = 0
   if number.is_a?(Integer)
-    puts "run: #{number}"
-    res = Array.new(number) {0}
-    if res.empty?
-      result = 0
-    else
-      res[0] = 1
-      res[1] = 1
-      for index in 2...res.size
-        res[index] = res [index - 2] + res[index - 1]
-      end
-      result = res.last
-    end
+    res = Array.new(number) { 0 }
+    return 0 if res.empty?
+    res[0] = 1
+    res[1] = 1
+    (2...res.size).each { |ind| res[ind] = res [ind - 2] + res[ind - 1] }
+    result = res.last
   end
   result
 end
@@ -42,25 +33,3 @@ def task_4_1(number)
   # use random for two version
   [true, false].sample ? task_4_1_a(number) : task_4_1_b(number)
 end
-
-
-
-puts task_4_1_a(0)
-puts task_4_1_a(1)
-puts task_4_1_a(2)
-puts task_4_1_a(3)
-puts task_4_1_a(4)
-puts task_4_1_a(5)
-puts task_4_1_a(6)
-
-puts '############################################'
-
-
-puts task_4_1_b(0)
-puts task_4_1_b(1)
-puts task_4_1_b(2)
-puts task_4_1_b(3)
-puts task_4_1_b(4)
-puts task_4_1_b(5)
-puts task_4_1_b(6)
-
