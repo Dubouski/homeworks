6d0cd6efa582c7fd73b63ea5ef65ba727c706469
Andrei Dubouski <dubouski.andrei@gmail.com>
2019-04-09 01:36:13 +0300

    Fix method 'task_4_1'.

--- HW_04/Andrei_Dubouski/andrei_dubouski_hw_04_t_01.rb
+++ HW_04/Andrei_Dubouski/andrei_dubouski_hw_04_t_01.rb
@@ -1,36 +1,18 @@
 def fib(number)
-  case number
-  when 0
-    0
-  when 1
-    1
-  else
-    fib(number - 1) + fib(number - 2)
-  end
-end
-
-def task_4_1_a(number)
-  # used recursion
-  res = 0
-  number.is_a?(Integer) && number >= 0 ? fib(number) : res
-end
-
-def task_4_1_b(number)
   # used dynamic programming
-  result = 0
-  if number.is_a?(Integer) && number >= 0
-    res = Array.new(number) { 0 }
-    return 0 if res.empty?
+  res = Array.new(number) { 0 }
+  return [] if res.empty?
 
-    res[0] = 1
-    res[1] = 1
-    (2...res.size).each { |ind| res[ind] = res [ind - 2] + res[ind - 1] }
-    result = res.last
-  end
-  result
+  res[0] = 1
+  res[1] = 1
+  (2...res.size).each { |ind| res[ind] = res [ind - 2] + res[ind - 1] }
+  res
 end
 
 def task_4_1(number)
-  # use random for two version
-  [true, false].sample ? task_4_1_a(number) : task_4_1_b(number)
+  if number.is_a?(Integer) && number >= 0
+    fib(number)
+  else
+    []
+  end
 end
