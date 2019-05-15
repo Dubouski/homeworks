32d119aab7077e2e424463d2b0dc03c0882ea366
Andrei Dubouski <dubouski.andrei@gmail.com>
2019-05-15 20:40:09 +0300

    Add new class Notifications.

---
+++ HW_05/Andrei_Dubouski/notification.rb
@@ -0,0 +1,29 @@
+# This class stores all notifications
+class Notifications
+  attr_accessor :all_notif
+
+  def initialize
+    @all_notif = []
+  end
+
+  def add_new(hw_name, st_name)
+    all_notif << { status: 'unread', student: st_name, hw_name: hw_name }
+  end
+
+  def to_s
+    output = []
+    all_notif.each do |n|
+      if n[:status].eql?('unread')
+        output << "Student #{n[:student]} has sent #{n[:hw_name]}"
+      end
+    end
+    output
+  end
+
+  def read
+    # set all notifications to "read" status"
+    all_notif.each do |n|
+      n[:status] = 'read' if n[:status].eql?('unread')
+    end
+  end
+end
---
+++ HW_05/Andrei_Dubouski/spec/notifications_spec.rb
@@ -0,0 +1,47 @@
+require 'rspec'
+require 'spec_helper'
+require_relative '../notification.rb'
+
+# Tests for all methods from the class Notifications
+RSpec.describe Notifications do
+  subject(:notifications) { Notifications.new }
+  let(:name) { 'Alice' }
+  let(:first_hw_name) { 'HW01' }
+  let(:result_to_s) { ['Student Alice has sent HW01'] }
+
+  describe '#initialize' do
+    it 'return zero length array' do
+      expect(notifications.all_notif.size).to eq 0
+    end
+  end
+
+  describe '#add_new' do
+    it 'add new notification' do
+      size = notifications.all_notif.size
+      notifications.add_new(first_hw_name, name)
+      expect(notifications.all_notif.size - size).to eq 1
+    end
+  end
+
+  describe '#to_s' do
+    it 'convert all notifications to string' do
+      notifications.add_new(first_hw_name, name)
+      expect(notifications.to_s).to eq(result_to_s)
+    end
+  end
+
+  describe '#read' do
+    it 'set all notifications to "read" status"' do
+      notifications.add_new(first_hw_name, name)
+      notifications.read
+      res_unread = 0
+      res_read = 0
+      notifications.all_notif.each do |note|
+        res_unread += 1 if note[:status].eql?('unread')
+        res_read += 1 if note[:status].eql?('read')
+      end
+      expect(res_unread).to eq(0)
+      expect(res_read).to eq(1)
+    end
+  end
+end
